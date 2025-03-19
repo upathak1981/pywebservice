06634c5d8c21cf1124fd937da6d6dfafb515038e
upathak1981 <95219280+upathak1981@users.noreply.github.com>
2025-03-19 12:43:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 @app.route('/')
 def rootfunction():
-    return 'First Python app on Heroku'    
+    return 'Hello my First Python app on Heroku'    
 if __name__=='__main__':
     app.run(debug=True)
     app.run()
